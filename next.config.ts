47e24b9a4139cfe06cc22d5cecd1335497d7af50
tej-carpenter <205124101@nitt.edu>
2026-05-04 23:15:41 +0530

    added images.pexels.com as host for images

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'images.unsplash.com' },
+      { protocol: 'https', hostname: 'images.pexels.com' },
       { protocol: 'https', hostname: 'firebasestorage.googleapis.com' },
       { protocol: 'https', hostname: 'blogger.googleusercontent.com' },
       { protocol: 'https', hostname: 'instagram.com' },
