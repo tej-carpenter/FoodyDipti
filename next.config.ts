8c596de0dafb657719dc13aa34f511441cafaae5
tej-carpenter <205124101@nitt.edu>
2026-05-09 14:40:20 +0530

    added cloudinary as image repository

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,7 @@ const nextConfig: NextConfig = {
       { protocol: 'https', hostname: 'instagram.com' },
       { protocol: 'https', hostname: 'cdninstagram.com' },
       { protocol: 'https', hostname: '4.ftcdn.net' },
+      { protocol: 'https', hostname: 'res.cloudinary.com' },
     ],
   },
 };
